f6491ca7bd134af3b084633c4864bf87eaca48e7
Jason Borowski <esc0rtd3w@gmail.com>
2016-03-17 23:48:58 -0400

    Added 2 Default "Platinum Edition" Examples For Char Replacement

--- gh-package-tool.js
+++ gh-package-tool.js
@@ -47,6 +47,16 @@ var scriptCredits = "\n\n\nesc0rtd3w / cRypTiCwaRe 2016";
 // RFS
 // http://games-dl.gamehouse.com/zylom/ghmigration/supergloveshero/f86bc49a788ace0058a420899e086139-supergloveshero.rfs
 
+
+// RFS Platinum Edition
+
+// Replace "platinumedition" with "pe"
+// http://games-dl.gamehouse.com/zylom/ghmigration/sablemazesullivanriverpe/5b8ef99d94b4cc0d4be3b7805188522f-sablemazesullivanriverpe.rfs
+
+// Replace Each word after 32 chars with just the first letter??
+// http://games-dl.gamehouse.com/zylom/ghmigration/deathuponanaustriansonataadknpe/af5adab1ea584472a88c2f32b111ec45-deathuponanaustriansonataadknpe.rfs
+
+
 // RGA (With Lang)
 // http://games-dl.gamehouse.com/zylom/popcap/dip_nt_zy_en/MONOPOLY.rga
 
