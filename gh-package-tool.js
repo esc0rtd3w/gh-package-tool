71d22e4e1261610d1d4038fca790bf8bfc6f719e
Jason Borowski <esc0rtd3w@gmail.com>
2016-03-31 15:13:47 -0400

    Re-ordered Developer List Array Alphabetically

--- gh-package-tool.js
+++ gh-package-tool.js
@@ -111,10 +111,10 @@ distributor.push("zylom");// Default Distributor After Migration
 var developer = [];
 developer.push("ghmigration");// Default Zylom Migration
 developer.push("artifex");
+developer.push("dekovir");
 developer.push("mumbo");
 developer.push("popcap");
 developer.push("terminalstudio");
-developer.push("dekovir");
 
 // Offering List
 var offering = [];
@@ -517,9 +517,9 @@ function createLinksRFS() {
 function createLinksRGA() {
 	// Samples
 	// http://games-dl.gamehouse.com/zylom/dekovir/dip_nt_zy_en/TradeMania2_EN.rga
-	linkRGALang = server[0] + "/" + distributor[0] + "/" + developer[2] + "/" + gameNamePackage + language[1] + "." + ext[1];
-	linkRGALangOffering = server[0] + "/" + distributor[0] + "/" + developer[5] + "/" + offering[0] + "/" + gameNamePackage + language[1] + "." + ext[1];
-	linkRGANoLang = server[0] + "/" + distributor[0] + "/" + developer[2] + "/" + gameNamePackage + "." + ext[1];
+	linkRGALang = server[0] + "/" + distributor[0] + "/" + developer[3] + "/" + gameNamePackage + language[1] + "." + ext[1];
+	linkRGALangOffering = server[0] + "/" + distributor[0] + "/" + developer[2] + "/" + offering[0] + "/" + gameNamePackage + language[1] + "." + ext[1];
+	linkRGANoLang = server[0] + "/" + distributor[0] + "/" + developer[3] + "/" + gameNamePackage + "." + ext[1];
 	linkRGALegacy = server[1] + path[4] + gameNamePackage + "/" + "am-" + gameNamePackage + "." + ext[1];
 	linkRGATrial = server[1] + path[3] + gameNamePackage + "/" + gameNamePackage + "." + ext[1];
 	linkRGAUnlimited = server[1] + path[5] + gameNamePackage + "/" + "amg-" + gameNamePackage + "." + ext[1];
