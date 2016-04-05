e990588c23dd32914a7c225253a18bf48a2f0f48
Jason Borowski <esc0rtd3w@gmail.com>
2016-04-05 03:05:12 -0400

    Added Batch Script To Grab Installation ID

---
+++ get-installation-id.cmd
@@ -0,0 +1,24 @@
+@echo off
+
+:: Part of the GameHouse Package Tool (Javascript)
+
+:: Parent Project: RealArcade Wrapper Killer
+:: esc0rtd3w / cRypTiC
+
+
+:: This script will retrieve the installation ID from an Acid 1.20.128.0 GameHouse Stub
+
+:: Requirements: gpatch.exe (included with RealArcade Wrapper Killer)
+
+set inputStub=%1
+
+cls
+echo %inputStub%
+echo.
+echo.
+pause>nul
+
+
+
+:end
+
