a41c3ae66cb9c1d4dea47bb9ae5f671124b66c51
Jason Borowski <esc0rtd3w@gmail.com>
2016-04-05 03:21:14 -0400

    Added sfk For Reading hex

--- get-installation-id.cmd
+++ get-installation-id.cmd
@@ -7,10 +7,14 @@
 
 
 :: This script will retrieve the installation ID from an Acid 1.20.128.0 GameHouse Stub
+:: The Installation ID can be extracted from any stub at hex offset 0x949CE - 0x949DD (16 Bytes)
 
-:: Requirements: gpatch.exe (included with RealArcade Wrapper Killer)
+:: Requirements: sfk.exe (included with RealArcade Wrapper Killer)
 
 set inputStub=%1
+
+set readID="%cd%\bin\sfk.exe" hexdump -pure -offlen 0x949CE 16
+
 
 cls
 echo %inputStub%
@@ -18,6 +22,7 @@ echo.
 echo.
 pause>nul
 
+%readID% "%inputStub%"
 
 
 :end
